8486f3a59729bb6cb5c05d00b70c7d1ece42c884
Piotr Brzezinski <01206120@pw.edu.pl>
2026-09-04 21:00:12 +0200

    begun refacroting Downloader class to work line by line. added requiremenst.txt

--- classes.py
+++ classes.py
@@ -6,45 +6,50 @@ from pathlib import Path
 class Downloader:
     def __init__(self, url: str):
         self.url = url
-        txt = re.search(r"/\d{9}/", self.url).group()
+        txt = re.search(r"/\d{9}/", self.url)
         if txt is None:
             raise Exception("ID number was not found")
-        self.data_name = txt.replace("/", "")
-        self.data = Downloader.download_url(self.url, self.data_name)
-        self.jpg_urls = []
-        dir_path = Path(self.data_name)
-        try:
-            dir_path.mkdir()
-        except FileExistsError:
-            pass
+        self._data_name = txt.group().strip('/')
+        self.dir = Path()
+        self.create_dir()
+        self._main_html = self.dir / self._data_name
+        self._main_html = self._main_html.with_suffix(".txt")
+
+    def create_dir(self):
+        self.dir = self.dir.cwd() / 'cache' / self._data_name
+        if not self.dir.exists():
+            self.dir.mkdir(parents=True)
 
 
     @staticmethod
-    def save_file(filename, contents):
-        with open(f"./cache/{filename}.txt", "w", encoding="utf-8") as file:
+    def save_file(filepath: Path, contents, mode="w"):
+        with open(filepath.as_uri(), mode=f"{mode}", encoding="utf-8") as file:
             for line in contents:
                 line = line + "\n"
                 file.write(line)
 
     @staticmethod
-    def save_file_bin(filename, contents):
-        with open(f"./cache/{filename}.txt", "wb", encoding="utf-8") as file:
-            for line in contents:
-                line = line + "\n"
-                file.write(line)
-
-    @staticmethod
-    def read_file(filename):
+    def read_file(filepath: Path, mode="r"):
+        if not filepath.exists():
+            print(f"file: {filepath.as_uri()} does not exist.")
+            return None
         try:
-            with open(f"./cache/{filename}.txt", "r", encoding="utf-8") as file:
+            with open(filepath.as_uri(), mode=f"{mode}", encoding="utf-8") as file:
                 output = file.readlines()
                 return output
         except Exception:
             return None
 
     @staticmethod
-    def download_url(url, filename):
-        data = Downloader.read_file(filename)
+    def request_url(url):
+        response = requests.get(url)
+        if response.status_code != 200:
+            raise Exception(f"could not request for {url} \nstatus code: {response.status_code}")
+        return response
+
+    def get_page_html(self):
+        Downloader.read_file(self._main_html)
+        data = Downloader.request_url(self.url)
         lines = []
         if data is not None:
             return data
@@ -71,7 +76,7 @@ class Downloader:
         for i in range(10):
             counter += 1
             response = requests.get(f"{self.jpg_urls[i]}")
-            with open(f"./{self.data_name}/{counter}.jpg", "wb") as file:
+            with open(f"./{self._data_name}/{counter}.jpg", "wb") as file:
                 file.write(response.content)
 
 
